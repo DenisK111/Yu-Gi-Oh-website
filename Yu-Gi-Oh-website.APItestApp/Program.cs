e477b5c792ab20e623f9bade1831483e12d38b6b
DenisK111 <denislavkraev@gmail.com>
2022-09-02 20:49:39 +0300

    Change the httpClient to a classLibrary and created a testApi Console app

---
+++ Yu-Gi-Oh-website.APItestApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System.Globalization;
+using Yu_Gi_Oh_website.Web.Data;
+
+namespace yu_gi_oh_website.httpclient
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var startDate = DateTime.ParseExact("01/01/2000", "MM/dd/yyyy", CultureInfo.InvariantCulture);
+            var endDate = DateTime.ParseExact(DateTime.UtcNow.Date.ToString("MM/dd/yyyy", CultureInfo.InvariantCulture), "MM/dd/yyyy", CultureInfo.InvariantCulture);
+            await new DbUpdater(new ApplicationDbContext()).DbUpdate("../../../Images", startDate, endDate);
+        }
+    }
+}
--- yu-gi-oh-website.httpclient/DbUpdater.cs
+++ yu-gi-oh-website.httpclient/DbUpdater.cs
@@ -14,85 +14,79 @@ namespace yu_gi_oh_website.httpclient
     public class DbUpdater
     {
         private readonly ApplicationDbContext context;
+      
 
-        public DbUpdater(ApplicationDbContext context )
+        public DbUpdater(ApplicationDbContext context)
         {
             this.context = context;
         }
-        public async Task DbUpdate(string imagesPath, DateTime inputStartDate, DateTime? inputEndDate = null)
+        public async Task DbUpdate(string imageFolder, DateTime inputStartDate, DateTime? inputEndDate = null)
         {
-            
+
             if (!inputEndDate.HasValue)
             {
                 inputEndDate = DateTime.Now;
             }
-            var httpClient = new HttpClient();
-            var apiParameters = new ApiCallParameters();
-            var result = await httpClient.GetAsync(apiParameters.GetAllCardsString(inputStartDate, inputEndDate));
+            using var httpClient = new HttpClient();
+            var apiParameters = new ApiCallString();
+            var result = await httpClient.GetAsync(apiParameters.GetAllTCGCardsString(inputStartDate, inputEndDate));
             result.EnsureSuccessStatusCode();
             string responseBody = await result.Content.ReadAsStringAsync();
 
             var json = JsonConvert.DeserializeObject<RootObject>(responseBody);
 
             //  File.WriteAllText("text.json", JsonConvert.SerializeObject(json));
-
-            using (httpClient)
+            foreach (var cardJson in json!.Data!)
             {
-                List<Card> cards = new List<Card>();
-                
-                foreach (var cardJson in json!.Data!)
+                var card = new Card()
                 {
-                   var card =  new Card()
+                    Name = cardJson.Name,
+                    Atk = cardJson.Atk,
+                    Def = cardJson.Def,
+                    Description = cardJson.Description,
+                    CardType = context.Types.FirstOrDefault(x => x.Name == cardJson.Type) ?? new CardType() { Name = cardJson.Type },
+                    Race = context.Races.FirstOrDefault(x => x.Name == cardJson.Race) ?? new Race() { Name = cardJson.Race },
+                    CardAttribute = cardJson.Attribute == null
+                     ? null
+                     : (context.CardAttributes.FirstOrDefault(x => x.Name == cardJson.Attribute)
+                     ?? new CardAttribute() { Name = cardJson.Attribute }),
+                    Scale = cardJson.Scale,
+                    LinkValue = cardJson.LinkValue,
+                    Level = cardJson.Level,
+                    HasEffect = cardJson.Misc[0].HasEffect,
+
+
+                };
+                await context.AddAsync(card);
+                await context.SaveChangesAsync();
+
+                var count = 1;                              
+                List<CardImage> cardImages = new List<CardImage>();
+                foreach (var link in cardJson.ImageUrls.Select(x => x.ImageUrl))
+                {
+                    string path = $"{imageFolder}/{cardJson.Name}{count++}.jpg";
+
+                    await DownloadImage(httpClient, link!, path);
+
+                    var cardImage = new CardImage()
                     {
-                        Name=cardJson.Name,
-                        Atk=cardJson.Atk,
-                        Def=cardJson.Def,
-                        Description=cardJson.Description,
-                        CardType= context.Types.FirstOrDefault(x=>x.Name == cardJson.Type) ?? new CardType() {Name=cardJson.Type },
-                        Race= context.Races.FirstOrDefault(x => x.Name == cardJson.Race) ?? new Race() { Name=cardJson.Race},
-                        CardAttribute = cardJson.Attribute == null 
-                        ? null
-                        : (context.CardAttributes.FirstOrDefault(x => x.Name == cardJson.Attribute)
-                        ?? new CardAttribute() {Name=cardJson.Attribute}),
-                        Scale = cardJson.Scale,
-                        LinkValue = cardJson.LinkValue,
-                        Level=cardJson.Level,
-                        HasEffect = cardJson.Misc[0].HasEffect,
-
-
+                        Card = card,
+                        ImageUrl = path
                     };
-                    await context.AddAsync(card);
-                    await context.SaveChangesAsync();
 
-                    var count = 1;
-                    var name = cardJson.Name;
-                    var listOfIMages = cardJson.ImageUrls.Select(x => x.ImageUrl);
-                    List<CardImage> cardImages = new List<CardImage>();
-                    foreach (var link in listOfIMages)
-                    {
-                        string path = $"{imagesPath}/{name}{count++}.jpg";
-
-                        await DownloadImage(httpClient, link!, path);
-
-                        var cardImage = new CardImage()
-                        {
-                            Card = card,
-                            ImageUrl = path
-                        };
-
-                        cardImages.Add(cardImage);
-                    }
-
-                    await context.AddRangeAsync(cardImages);
-                    await context.SaveChangesAsync();
+                    cardImages.Add(cardImage);
                 }
-                    
+
+                await context.AddRangeAsync(cardImages);
+                await context.SaveChangesAsync();
             }
+
+
         }
         private async Task DownloadImage(HttpClient client, string url, string path)
         {
-            var array = await client.GetByteArrayAsync(url);
-            await File.WriteAllBytesAsync(path, array);
+            var byteArray = await client.GetByteArrayAsync(url);
+            await File.WriteAllBytesAsync(path, byteArray);
 
         }
     }
