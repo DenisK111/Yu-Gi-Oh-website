65cda5be235c33ce97cc69a9887416fc6f3a7ac0
DenisK111 <denislavkraev@gmail.com>
2022-12-11 14:29:26 +0200

    downloaded static images to be hosted locally

--- Yu-Gi-Oh-website.Data/Data/Seeding/ImageUrls.cs
+++ Yu-Gi-Oh-website.Data/Data/Seeding/ImageUrls.cs
@@ -4,10 +4,10 @@
     {
         //Cattegories
 
-        public static string Anime = "https://i.pinimg.com/originals/0e/72/b3/0e72b35e99486084d2170a8d7cc38394.png";
-        public static string MasterDuel = "https://img.konami.com/kde_cms/eu_publish/uploads/masterduellogo_169-600x338.png";
-        public static string DuelLinks = "https://www.kindpng.com/picc/m/363-3633075_yu-gi-oh-duel-links-logo-hd-png.png";
-        public static string TCG = "https://www.pngitem.com/pimgs/m/172-1722626_4thtcgen-zpsa403481d-yu-gi-oh-trading-card-game.png";
-        public static string Miscellaneous = "https://static.vecteezy.com/system/resources/previews/005/155/686/non_2x/arabesque-web-horizontal-banner-camel-and-palm-tree-silhouette-beautiful-sunlight-sunset-sunrise-islamic-background-template-illustration-vector.jpg";
+        public static string Anime = "static/anime.png";
+        public static string MasterDuel = "static/masterDuel.png";
+        public static string DuelLinks = "static/duellinks.png";
+        public static string TCG = "static/tcg.png";
+        public static string Miscellaneous = "static/misc.jpg";
     }
 }
